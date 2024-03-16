7312030329ddcb95ef30bc13ad2f0de94c9cc937
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-16 23:20:43 +0200

    Navbar Layout Structure
    Additional media content: Navbar logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/home';
+import { Navbar } from './components/navigation/navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
---
+++ src/components/navigation/navbar/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import logo from '../../../assets/img/gg_logo.png';
+import './index.css'
+
+export const Navbar = () => {
+    const [activePage, setActivePage] = useState('/')
+    const pages = [
+        {
+            title: 'Home',
+            url: '/'
+        },
+        {
+            title: 'Destinations',
+            url: '/destinations'
+        }
+    ]
+
+    return (
+        <div className='navbar'>
+            <div className='navbar-left'>
+                <img src={logo} alt='Logo' />
+                <ul className='navbar-left-items'>
+                    {
+                        pages.map((page) => {
+                            return (
+                                <li className={`navbar-left-item ${page.url === activePage ? 'active-page' : ''}`}
+                                    onClick={() => { setActivePage(page.url) }}>
+                                    {page.title}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            <div className='navbar-right'>
+                <button>Get the App</button>
+            </div>
+        </div>
+    )
+}
