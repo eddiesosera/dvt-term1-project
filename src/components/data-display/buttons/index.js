e280e437684ae744ea92484c8f18883a3db780ee
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-17 00:16:16 +0200

    Hero Content Structure + Button Fill extra props

--- src/components/data-display/buttons/index.js
+++ src/components/data-display/buttons/index.js
@@ -13,12 +13,13 @@ export const ButtonOutline = ({ borderColor, borderWith, textColor, fontSize, ch
     )
 }
 
-export const ButtonFill = ({ backgroundColor, textColor, fontSize, children }) => {
+export const ButtonFill = ({ backgroundColor, textColor, fontSize, width, children }) => {
     return (
         <div className='gg-button' style={{
             background: backgroundColor,
             color: textColor,
-            background: 'none', fontSize: `${fontSize}px`
+            fontSize: fontSize,
+            // width: width | 'max-content'
         }}>
             {children}
         </div>
--- src/pages/home/sections/hero/index.js
+++ src/pages/home/sections/hero/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './index.css';
 import heroBackground from '../../../../assets/img/header_background.png';
+import { ButtonFill } from '../../../../components/data-display/buttons';
 
 export const HeroSection = () => {
     return (
-        <div >
+        <div className='hero-section'>
             <img className='hero-background-img' src={heroBackground} alt='Background' />
+            <div className='hero-content'>
+                <div className='hero-content-top'>
+                    Tired Of Global Warming?
+                </div>
+                <div className='hero-content-heading'>
+                    Visit A <br />Different Planet
+                </div>
+                <div className='hero-content-description'>
+                    Welcome to Celestial Voyages, where your dreams of interplanetary exploration come to life. Join us in venturing beyond the stars, discovering the wonders of otherworldly landscapes.
+                </div>
+                <div className='hero-content-cta'>
+                    <ButtonFill backgroundColor='#7920C2' textColor='#FFFFFF' fontSize={16}>
+                        Destination
+                    </ButtonFill>
+                </div>
+            </div>
         </div>
     )
 }
