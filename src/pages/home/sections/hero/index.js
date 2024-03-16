d0e8694f221f7929713baffd2b435168e5d10f0f
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-16 22:58:45 +0200

    Pages and Section files setup + Header bg set

---
+++ src/pages/home/sections/hero/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import heroBackground from '../../../../assets/img/header_background.png';
+
+export const HeroSection = () => {
+    return (
+        <div>
+            <img className='hero-background-img' src={heroBackground} alt='Background' />
+        </div>
+    )
+}
